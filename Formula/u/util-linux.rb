0e1e7d00cda1d28aa14d09fbaf583bfa07a65408
Rui Chen <rui@chenrui.dev>
2024-03-28 23:21:19 -0400

    util-linux 2.40
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/util-linux.rb
+++ Formula/u/util-linux.rb
@@ -1,8 +1,8 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/util-linux/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.39/util-linux-2.39.3.tar.xz"
-  sha256 "7b6605e48d1a49f43cc4b4cfc59f313d0dd5402fa40b96810bd572e167dfed0f"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.40/util-linux-2.40.tar.xz"
+  sha256 "d57a626081f9ead02fa44c63a6af162ec19c58f53e993f206ab7c3a6641c2cd7"
   license all_of: [
     "BSD-3-Clause",
     "BSD-4-Clause-UC",
